aeca67385edd12d8ad9a82ad5e2423172a19bcaa
psriharsha <mail@psriharsha.me>
2018-07-05 15:00:44 +0100

    Sortable implentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SplitPaneModule } from 'ng2-split-pane/lib/ng2-split-pane';
 import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { SortablejsModule } from 'angular-sortablejs';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
@@ -23,7 +24,8 @@ import { ModalComponent } from './modal.component';
     BrowserModule,
     SplitPaneModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SortablejsModule.forRoot({ animation: 150 })
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
